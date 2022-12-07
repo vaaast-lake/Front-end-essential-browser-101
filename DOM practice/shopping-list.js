d5836143b1895899cc7660eeb010e19bf612926a
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2022-12-07 16:54:05 +0900

    add button
    button synchronize with strickthrough.
    
    stickthrough work when user clicks item box or text.

--- DOM practice/shopping-list.js
+++ DOM practice/shopping-list.js
@@ -9,25 +9,32 @@ const items = document.querySelector('.items');
 inputData.addEventListener('keyup', (el) => {
 
   // createElement
-  const item = document.createElement('span');
+  const item = document.createElement('div');
+  const itemLeft = document.createElement('span');
+  const itemRight = document.createElement('span');
   const deleteAnchor = document.createElement('a');
   const trashIcon = document.createElement('i');
-  const strickBtn = document.createElement('button');
+  const strickBtn = document.createElement('input');
 
   // add item
   const addItem = (value) => {
 
     item.setAttribute('class', 'item');
     
+    itemLeft.setAttribute('class', 'item__left');
+    itemRight.setAttribute('class', 'item__right');
+    strickBtn.setAttribute('type', 'checkbox');
     strickBtn.setAttribute('class', 'strickBtn');
     
     deleteAnchor.setAttribute('class', 'delete');
     trashIcon.setAttribute('class', 'fa-solid fa-trash-can');
     
     items.append(item);
-    item.append(strickBtn);
-    item.textContent = `${value}`;
-    item.append(deleteAnchor);
+    item.append(itemLeft);
+    itemLeft.append(strickBtn);
+    itemLeft.append(`${value}`);
+    item.append(itemRight);
+    itemRight.append(deleteAnchor);
     deleteAnchor.append(trashIcon);
   }
 
@@ -42,52 +49,50 @@ inputData.addEventListener('keyup', (el) => {
     value.remove();
   }
 
-  
   trashIcon.addEventListener('click', (el) => {
-    deleteItem(el.target.parentNode.parentNode);
+    deleteItem(el.target.parentNode.parentNode.parentNode);
   });
   
 });
 
 const controlStrickthrough = (node) => {
   const strickthrough = document.createElement('s');
-  const nodeList = node.childNodes;
-  const textNode = nodeList[0];
-  const anchorNode = nodeList[1];
+  const leftNode = node[0];
+  const button = leftNode.childNodes[0];
+  const textNode = leftNode.childNodes[1];
 
-  // console.log('nodeType: ', textNode.nodeType);
-  
   // type 3 = text;
   if (textNode.nodeType === 3) {
-    node.removeChild(textNode);
-    node.insertBefore(strickthrough, anchorNode);
+    leftNode.removeChild(textNode);
+    leftNode.append(strickthrough);
     strickthrough.append(textNode);
+    // 취소선이 들어가면 버튼도 체크된다.
+    button.checked = true;
     // type 1 = element;
   } else if (textNode.nodeType === 1) {
     const pureText = textNode.childNodes[0];
-    node.removeChild(textNode);
-
-    // console.log(pureText.nodeType);
-    // console.log(nodeList[0].nodeType);
-
-    anchorNode.before(pureText);
-
-    // console.log('afterNodeList: ', nodeList);
-    // console.log('pure: ', pureText);
-    // console.log(anchorNode.nodeType);
-    // return [pureText, nodeList];
+    leftNode.removeChild(textNode);
+    leftNode.append(pureText);
+    button.checked = false;
   }
 }
 
 // if click, string add strickthrough
-// inputData listener 안에 넣으면 반복해서 실행된다.
 items.addEventListener('click', (el) => {
   const node = el.target;
+
   if (node.nodeName !== 'I') {
-    // 취소선이 들어갔을 때 contents 안에 text를 클릭하면 취소선이 중첩해서 들어가는 버그가 있었다.
-    // 취소선이 들어갔을 때는 취소선 노드의 부모를 넣어주고, 정상적으로 span node 가 node 로 할당됐으면 그대로 넣어줬다. 
-    node.nodeName === 'S' ? controlStrickthrough(node.parentNode) : controlStrickthrough(node);
-    // console.log('nodeName: ', node.nodeName);
-    // console.log('nodeParent: ', node.parentNode);
+
+    // 체크박스를 누르거나 이미 취소선이 그어진 텍스트를 눌렀을 때 보내야 할 노드를 구분했다.
+    if (node.nodeName === 'S' || node.nodeName === 'INPUT') {
+      controlStrickthrough(node.parentNode.parentNode.childNodes);
+      // 아이템 박스를 클릭하거나 텍스트를 클릭했을 때 보낼 노드를 구분해줬다.
+    } else if (node.nodeName === 'SPAN') {
+      console.log(node.parentNode.childNodes);
+      controlStrickthrough(node.parentNode.childNodes);
+    } else if (node.nodeName === 'DIV') {
+      controlStrickthrough(node.childNodes);
+    }
+
   }
 });
