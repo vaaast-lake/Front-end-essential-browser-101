11fd803f0f2041598f75fca0c1f669a4bea17394
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2022-12-10 13:32:14 +0900

    add localstorage in shopping-list

--- 5. DOM practice/shopping-list-dream.js
+++ 5. DOM practice/shopping-list-dream.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/**
- * 1. 사용자가 텍스트 인풋에서 타이핑 할 수 있다
- * 2. 아이템 추가는 플러스 버튼이나 텍스트에서 엔터를 했을 때 가능하다.
- * 아이템이 등록되면 쓰레기 아이콘으로 삭제할 수 있다. 
- */
-
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
@@ -16,27 +10,20 @@ form.addEventListener('submit', event => {
   onAdd();
 });
 
-// 클릭해서 발생한 이벤트를 처리하는 함수는 보통 on을 붙인다.
 const onAdd = () => {
-  // 1. 사용자가 입력한 텍스트를 받아옴.
   const text = input.value;
   if (text === '') {
     input.focus();
     return;
   }
-  // 2. 받아온 텍스트로 새로운 아이템을 만든다.(텍스트 + 삭제 버튼)
   const item = createItem(text);
-  // 3. items 컨테이너 안에 새로 만든 아이템을 추가한다.
   items.appendChild(item);
-  // 4. 새로 추가된 아이템으로 스크롤링
   item.scrollIntoView({block: 'center'});
-  // 5. 인풋을 초기화 한다.
   input.value = '';
-  // 입력 후 다시 인풋에 포커스를 줘서 재입력하기 용이하게 만듦
   input.focus();
 }
 
-let id = 0; // production level 에서는 UUID(unique id)나 obj의 hash code 로 고유한 아이디를 만들지만, 간단한 프로젝트라 숫자로 대체한다. 
+let id = 0;
 const createItem = (text) => {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
@@ -50,57 +37,40 @@ const createItem = (text) => {
     </div>
     <div class="item__divider"></div>
   `;
+
+  localStorage.setItem(id, itemRow.outerHTML);
+
   ++id;
-
-  // itemRow.addEventListener('click', () => {
-  //   if (event.target.tagName === "I") {
-  //     items.removeChild(itemRow);
-  //   }
-  // });
-
-  // const item = document.createElement('div');
-  // item.setAttribute('class', 'item');
-
-  // const name = document.createElement('span');
-  // name.setAttribute('class', 'item__name');
-  // name.innerText = text;
-
-  // const deleteBtn = document.createElement('button');
-  // deleteBtn.setAttribute('class', 'item__delete');
-  // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  // deleteBtn.addEventListener('click', () => {
-  //   items.removeChild(itemRow);
-  // });
-
-  // const itemDivider = document.createElement('div');
-  // itemDivider.setAttribute('class', 'item__divider');
-
-  // item.appendChild(name);
-  // item.appendChild(deleteBtn);
-
-  // itemRow.appendChild(item);
-  // itemRow.appendChild(itemDivider);
 
   return itemRow;
 }
 
-// addBtn.addEventListener('click', () => {
-//   onAdd();
-// });
+const listStoredItems = () => {
+  // methods in localstorage is gotten as key too. They make null when JSON.parse() is running.
+  // for (const key in localStorage) {
+  //   const storedItem = JSON.parse(localStorage.getItem(key));
+  //   items.insertAdjacentHTML('beforeend', storedItem);
+  // }
+  for (const key in localStorage) {
+    // methods in localstorage assigned prototype method. So, if run for...in loop, method is printed as key. Object.hasOwnProperty() prevents prototype methods being printed.
+    if (Object.hasOwnProperty.call(localStorage, key)) {
+      const storedItem = localStorage[key];
+      // console.log(storedItem);
+      items.insertAdjacentHTML('beforeend', storedItem);
+    }
+  }
+}
 
-// input.addEventListener('keydown', (event) => {
-//   // if (event.isComposing) {
-//   //   return;
-//   // }
-//   if (event.key === 'Enter') {
-//     onAdd();
-//   }
-// });
-
+// 삭제
 items.addEventListener('click', event => {
   const id = event.target.dataset.id;
   if (event.target.dataset.id) {
     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
     toBeDeleted.remove();
+    localStorage.removeItem(id);
   }
 });
+
+if (localStorage.getItem('0')) {
+  listStoredItems();
+}
