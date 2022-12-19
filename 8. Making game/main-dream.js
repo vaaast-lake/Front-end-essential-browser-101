23e164a97cef5eb76cd92f66368271f042be7684
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2022-12-19 17:25:15 +0900

    CSS, JS: random positioned carrot & bug by Ellie

--- 8. Making game/main-dream.js
+++ 8. Making game/main-dream.js
@@ -1,34 +1,35 @@
 'use strict';
 
+const CARROT_SIZE = 80;
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
-const randomCoordinate = (maxHorizontal, maxVertical) => {
-  const randomHorizontal = Math.floor(Math.random() * (maxHorizontal + 1));
-  const randomVertical = Math.floor(Math.random() * (maxVertical + 1));
-  return [randomHorizontal, randomVertical];
+const randomNumber = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
 }
 
 const initGame = () => {
-  const bugMaxHorizontal = fieldRect.width - 50;
-  const bugMaxVertical = fieldRect.height - 50;
-  const carrotMaxHorizontal = fieldRect.width - 80;
-  const carrotMaxVertical = fieldRect.height - 80;
-  
-  for (let i = 0; i < 5; i++) {
-    const bug = document.createElement('img');
-    const carrot = document.createElement('img');
-    const [bugX, bugY] = randomCoordinate(bugMaxHorizontal, bugMaxVertical);
-    const [carrotX, carrotY] = randomCoordinate(carrotMaxHorizontal, carrotMaxVertical);
-    bug.setAttribute('class', 'bug');
-    bug.setAttribute('src', './carrot/img/bug.png');
-    carrot.setAttribute('class', 'carrot');
-    carrot.setAttribute('src', './carrot/img/carrot.png');
-   
-    bug.style.transform = `translate(${bugX}px, ${bugY}px)`;
-    carrot.style.transform = `translate(${carrotX}px, ${carrotY}px)`;
+  addItem('carrot', 5, './carrot/img/carrot.png');
+  addItem('carrot', 5, './carrot/img/bug.png');
+}
 
-    field.append(bug, carrot);
+const addItem = (className, count, imgPath) => {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
   }
 }
 
